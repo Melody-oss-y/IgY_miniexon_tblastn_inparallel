822f9f208c244bcba3676e1398e9bbbb9ad8fb6c
melody-oss-y <melody.xiner.wu@gmail.com>
2026-03-14 17:28:16 -0600

    adding Sankey Diagram

---
+++ SankeyDiagram.R
@@ -0,0 +1,28 @@
+// Enter Flows between Nodes, like this:
+  //         Source [AMOUNT] Target
+
+Birds [281] Birds w/ NCBI RefSeq #e6ab02
+Birds [13] Birds w/o NCBI RefSeq #bebada
+:Birds #e6ab02
+
+Birds w/o NCBI RefSeq [13]
+Birds w/ NCBI RefSeq [219] IgY HC hits #edc549
+Birds w/ NCBI RefSeq [62] IgY HC no hits #bebada
+:Birds w/ NCBI RefSeq #edc549
+:Birds w/o NCBI RefSeq #bebada
+:IgY HC hits #edc549
+:IgY HC no hits #bebada
+
+IgY HC hits [45] mini-exon present #1b9b77
+IgY HC hits [64] mini-exon absent #e15759
+IgY HC hits [110] no alignment to \n CH2 - CH3 intron #bebada
+:no alignment to \n CH2 - CH3 intron #bebada
+:mini-exon present #1b9b77
+:mini-exon absent #e15759
+
+mini-exon present [45] Anseriformes #17becf
+:Anseriformes #17becf
+
+mini-exon absent [6] Anseriformes
+mini-exon absent [58] non-Anseriformes Birds #af7aa1
+:non-Anseriformes Birds #af7aa1
